163b15033b8ae5563674663b64a55162eaec8d00
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-02 14:26:26 +0100

    implemented prettystring

--- src/main/java/no/uib/inf101/tetris/model/TetrisBoard.java
+++ src/main/java/no/uib/inf101/tetris/model/TetrisBoard.java
@@ -1,6 +1,7 @@
 package no.uib.inf101.tetris.model;
 
 import no.uib.inf101.grid.Grid;
+import no.uib.inf101.grid.GridCell;
 
 /**
  * Objects in this class represents a tetris board.
@@ -19,4 +20,22 @@ public class TetrisBoard extends Grid<Character> {
         super(rows, cols, '-');
     }
 
+    /**
+     * Method for displaying the values of the tetris board as a string.
+     * 
+     * @return a string representation of the board
+     */
+
+    public String prettyString() {
+        String board = "";
+        Integer count = 1;
+        for (GridCell<Character> cell : this) {
+            board += cell.value();
+            if (count % this.cols() == 0) {
+                board += "\n";
+            }
+            count += 1;
+        }
+        return board.substring(0, board.length() - 1);
+    }
 }
