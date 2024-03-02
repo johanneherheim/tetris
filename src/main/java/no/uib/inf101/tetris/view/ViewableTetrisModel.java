ec9e705271d7ab6b92c1629c27ca187110fec169
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-02 13:55:28 +0100

    javadocs

--- src/main/java/no/uib/inf101/tetris/view/ViewableTetrisModel.java
+++ src/main/java/no/uib/inf101/tetris/view/ViewableTetrisModel.java
@@ -4,7 +4,19 @@ import no.uib.inf101.grid.GridCell;
 import no.uib.inf101.grid.GridDimension;
 
 public interface ViewableTetrisModel {
+    /**
+     * Gives us the size of the tetrisboard,
+     * and GridDimension consist of rows and cols
+     * 
+     * @return a gridDimension object
+     */
     GridDimension getDimension();
 
+    /**
+     * Gives us all the posisions of the tiles on the board
+     * 
+     * @return Iterable<GridCell<Character>> which iterates over all the tiles on
+     *         the board
+     */
     Iterable<GridCell<Character>> getTilesOnBoard();
 }
