9f8feb1079f94beb207d265f34360750bad16c84
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-02 19:12:28 +0100

    getNext()

--- src/main/java/no/uib/inf101/tetris/model/tetromino/RandomTetrominoFactory.java
+++ src/main/java/no/uib/inf101/tetris/model/tetromino/RandomTetrominoFactory.java
@@ -1,5 +1,14 @@
 package no.uib.inf101.tetris.model.tetromino;
 
-public class RandomTetrominoFactory {
+import java.util.Random;
 
+public class RandomTetrominoFactory implements TetrominoFactory {
+
+    @Override
+    public Tetromino getNext() {
+        String tetrominoTypes = "LJSZTIO";
+        char randomType = tetrominoTypes.charAt(new Random().nextInt(tetrominoTypes.length()));
+
+        return Tetromino.newTetromino(randomType);
+    }
 }
