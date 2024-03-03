c1b0a4b3d0958a68e9dc47a54ffe96fcc3c0fc07
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-03 15:18:27 +0100

    Implemented keyPressed in TetrisController

--- src/main/java/no/uib/inf101/tetris/controller/TetrisController.java
+++ src/main/java/no/uib/inf101/tetris/controller/TetrisController.java
@@ -1,13 +1,47 @@
 package no.uib.inf101.tetris.controller;
 
+import java.awt.event.KeyEvent;
+
 import no.uib.inf101.tetris.view.TetrisView;
 
-public class TetrisController {
+public class TetrisController implements java.awt.event.KeyListener {
     ControllableTetrisModel controllableTetrisModel;
     TetrisView tetrisView;
 
-    TetrisController(ControllableTetrisModel controllableTetrisModel, TetrisView tetrisView) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public TetrisController(ControllableTetrisModel controllableTetrisModel, TetrisView tetrisView) {
+        this.controllableTetrisModel = controllableTetrisModel;
+        this.tetrisView = tetrisView;
+        tetrisView.addKeyListener(this);
+        tetrisView.setFocusable(true);
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            controllableTetrisModel.moveTetromino(0, -1);
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            controllableTetrisModel.moveTetromino(0, 1);
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            controllableTetrisModel.moveTetromino(1, 0);
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            controllableTetrisModel.moveTetromino(-1, 0);
+        } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            // Spacebar was pressed
+        }
+        tetrisView.repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
     }
 
 }
