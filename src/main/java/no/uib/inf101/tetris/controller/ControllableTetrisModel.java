d22f8904cab0c600e3c90e02b3bd85aaca9c9bf3
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-03 12:30:27 +0100

    added controller package

---
+++ src/main/java/no/uib/inf101/tetris/controller/ControllableTetrisModel.java
@@ -0,0 +1,4 @@
+package no.uib.inf101.tetris.controller;
+
+public interface ControllableTetrisModel {
+}
---
+++ src/main/java/no/uib/inf101/tetris/controller/TetrisController.java
@@ -0,0 +1,5 @@
+package no.uib.inf101.tetris.controller;
+
+public class TetrisController {
+
+}
