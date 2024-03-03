70bbb87bd209f43f323bf90f9c6395fd50e60c4f
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-03 14:14:58 +0100

    added constructior to TetrisController

--- src/main/java/no/uib/inf101/tetris/controller/TetrisController.java
+++ src/main/java/no/uib/inf101/tetris/controller/TetrisController.java
@@ -1,5 +1,13 @@
 package no.uib.inf101.tetris.controller;
 
+import no.uib.inf101.tetris.view.TetrisView;
+
 public class TetrisController {
+    ControllableTetrisModel controllableTetrisModel;
+    TetrisView tetrisView;
+
+    TetrisController(ControllableTetrisModel controllableTetrisModel, TetrisView tetrisView) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 
 }
