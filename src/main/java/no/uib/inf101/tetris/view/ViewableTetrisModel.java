9e5bb779e3bf4d09b00aee4ed32f5bedc89a89fc
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-04 14:52:28 +0100

    updated ViewableTetrisModel interface with getGameState method

--- src/main/java/no/uib/inf101/tetris/view/ViewableTetrisModel.java
+++ src/main/java/no/uib/inf101/tetris/view/ViewableTetrisModel.java
@@ -2,6 +2,7 @@ package no.uib.inf101.tetris.view;
 
 import no.uib.inf101.grid.GridCell;
 import no.uib.inf101.grid.GridDimension;
+import no.uib.inf101.tetris.model.GameState;
 
 /** This interface has all the methods that a tetrismodel should have */
 public interface ViewableTetrisModel {
@@ -29,4 +30,12 @@ public interface ViewableTetrisModel {
      */
 
     Iterable<GridCell<Character>> fallingTetromino();
+
+    /**
+     * Gives us the current gamestate
+     * 
+     * @return the current gamestate
+     */
+
+    GameState getGameState();
 }
