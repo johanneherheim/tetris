92c2213259f0135b002240a4832d5ff9757ad950
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-04 14:52:34 +0100

    updated ViewableTetrisModel interface with getGameState method

--- src/main/java/no/uib/inf101/tetris/controller/ControllableTetrisModel.java
+++ src/main/java/no/uib/inf101/tetris/controller/ControllableTetrisModel.java
@@ -1,9 +1,35 @@
 package no.uib.inf101.tetris.controller;
 
+import no.uib.inf101.tetris.model.GameState;
+
 public interface ControllableTetrisModel {
+
+    /**
+     * Moves the tetromino
+     * 
+     * @param deltaRow the change in row
+     * @param deltaCol the change in column
+     * @return true if the tetromino was moved, false if not
+     */
     boolean moveTetromino(int deltaRow, int deltaCol);
 
+    /**
+     * Rotates the tetromino clockwise
+     * 
+     * @return true if the tetromino was rotated, false if not
+     */
     boolean rotateTetromino();
 
+    /**
+     * Drops the tetromino to the bottom of the board
+     */
     void dropTetromino();
+
+    /**
+     * Gives us the current gamestate
+     * 
+     * @return the current gamestate
+     */
+
+    GameState getGameState();
 }
