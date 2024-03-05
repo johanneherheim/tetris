f959064950f1d0eee401539bdeb15e3eaf27aa69
Johanne Blikberg Herheim <joher2128@uib.no>
2024-03-05 09:21:56 +0100

    added test for removing rows (DOES NOT PASS)

--- src/test/java/no/uib/inf101/tetris/model/TetrisBoardTest.java
+++ src/test/java/no/uib/inf101/tetris/model/TetrisBoardTest.java
@@ -22,4 +22,40 @@ public class TetrisBoardTest {
         assertEquals(expected, board.prettyString());
     }
 
+    @Test
+    public void testRemoveFullRows() {
+        // Tester at fulle rader fjernes i brettet:
+        // -T
+        // TT
+        // LT
+        // L-
+        // LL
+
+        TetrisBoard board = new TetrisBoard(5, 2);
+        board.set(new CellPosition(0, 1), 'T');
+        board.set(new CellPosition(1, 0), 'T');
+        board.set(new CellPosition(1, 1), 'T');
+        board.set(new CellPosition(2, 1), 'T');
+        board.set(new CellPosition(4, 0), 'L');
+        board.set(new CellPosition(4, 1), 'L');
+        board.set(new CellPosition(3, 0), 'L');
+        board.set(new CellPosition(2, 0), 'L');
+
+        assertEquals(3, board.removeFullRows());
+
+        String expected = String.join("\n", new String[] {
+                "--",
+                "--",
+                "--",
+                "-T",
+                "L-"
+        });
+        assertEquals(expected, board.prettyString());
+    }
+
+    // test der nederste rad må beholdes
+
+    // test der øverste rad skal fjernes
+
+    // test med en annen bredde på brettet
 }
